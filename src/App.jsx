61acb9a9b509637523309aa32ab8ff74ed5ea852
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-02-03 15:28:31 -0300

    updated and add a useEffect hook to an api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import React from 'react';
 import { MainStyled } from './components/Main.style';
 import Slider from './Slider';
 
 function App() {
+   const [datas, setDatas] = React.useState(null);
+   React.useEffect(() => {
+      fetch(
+         'https://db.ygoprodeck.com/api/v7/cardinfo.php?cardset=metal%20raiders&num=5&offset=0',
+      )
+         .then((res) => res.json())
+         .then((json) => setDatas(json.data));
+   }, []);
+
+   console.log(datas);
+
    return (
       <>
          <MainStyled>
-            <Slider />
+            <Slider datas={datas} />
          </MainStyled>
       </>
    );
