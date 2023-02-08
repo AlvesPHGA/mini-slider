4e2546c9d5876760e98986dcf329c5cb4ab37cd8
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-02-08 14:44:43 -0300

    updated Slider

--- src/Slider.jsx
+++ src/Slider.jsx
@@ -1,76 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-
-const SectionStyle = styled.section`
-   margin: 0 auto;
-   width: 600px;
-   overflow: hidden;
-`;
-
-const SliderStyle = styled.div`
-   display: flex;
-   flex-flow: row nowrap;
-`;
-
-const CardStyle = styled.div`
-   flex-shrink: 0;
-   width: 100%;
-   display: grid;
-   justify-content: space-between;
-   grid-template-columns: 1fr 1fr;
-   padding: 0.25rem;
-   border-radius: 0.5rem;
-   background: rgb(231, 233, 234);
-   gap: 0.25rem;
-`;
-
-const CardStyle__img = styled.div`
-   height: 400px;
-
-   & img {
-      width: 100%;
-      height: 100%;
-      border-radius: 0.5rem;
-   }
-`;
-
-const CardStyle__content = styled.div`
-   padding: 0.75rem 0 0.75rem 0.25rem;
-   background: rgba(255, 255, 255, 0.6);
-   border-radius: 0.5rem;
-   & h2 {
-      font-weight: 700;
-      font-size: 2rem;
-      margin-bottom: 0.75rem;
-   }
-`;
-
-const CardStyle__description = styled.div`
-   margin: 0.75rem 0;
-   & p {
-      font-weight: 600;
-      font-size: 1.25rem;
-   }
-`;
-
-const CardStyle__information = styled.div`
-   & h3 {
-      font-size: 1.5rem;
-   }
-   & ul {
-      list-style: none;
-
-      & li {
-         font-size: 1.25rem;
-         font-weight: 500;
-         margin-top: 0.25rem;
-
-         & span {
-            font-weight: 600;
-         }
-      }
-   }
-`;
+import { SectionStyle } from './style/Slider.styled';
 
 const ButtonsStyle = styled.div`
    display: flex;
@@ -87,22 +17,40 @@ const ButtonsStyle = styled.div`
 
 const Slider = ({ datas }) => {
    const [activeSlide, setActiveSlide] = React.useState(0);
+   const [positionItemSlider, setPositionItemSlider] = React.useState(0);
 
-   function previousSlider(targ) {
-      setActiveSlide(activeSlide - 1);
+   const refSlider = React.useRef();
+
+   React.useEffect(() => {
+      const { width } = refSlider.current.getBoundingClientRect();
+
+      setPositionItemSlider(-(width * activeSlide));
+   }, [activeSlide]);
+
+   function previousSlider() {
+      // setActiveSlide(activeSlide - 1);
+      // if (activeSlide === 0) setActiveSlide(datas.length - 1);
+      activeSlide === 0
+         ? setActiveSlide(datas.length - 1)
+         : setActiveSlide(activeSlide - 1);
    }
 
-   function nextSlider(targ) {
-      setActiveSlide(activeSlide + 1);
-   }
-
-   function handleClick({ target }) {
-      target.id === 'previous' ? previousSlider() : nextSlider();
+   function nextSlider() {
+      // if (activeSlide < datas.length - 1) setActiveSlide(activeSlide + 1);
+      // if (activeSlide === datas.length - 1) setActiveSlide(0);
+      // else setActiveSlide(activeSlide + 1);
+      activeSlide === datas.length - 1
+         ? setActiveSlide(0)
+         : setActiveSlide(activeSlide + 1);
    }
 
    return (
       <SectionStyle>
-         <SliderStyle>
+         <div
+            ref={refSlider}
+            className="Slider"
+            style={{ transform: `translateX(${positionItemSlider}px)` }}
+         >
             {datas.map(
                ({
                   id,
@@ -115,19 +63,20 @@ const Slider = ({ datas }) => {
                   atk,
                   def,
                }) => (
-                  <CardStyle key={id}>
-                     <CardStyle__img key={card_images[0]}>
+                  <div className="Slider__card" key={id}>
+                     <div className="Slider__img" key={card_images[0]}>
                         <img
+                           className="imgCard"
                            src={card_images[0].image_url}
                            alt={`Card ${name}`}
                         />
-                     </CardStyle__img>
-                     <CardStyle__content>
+                     </div>
+                     <div className="Slider__content">
                         <h2>{name}</h2>
-                        <CardStyle__description>
+                        <div className="Slider__description">
                            <p>{desc}</p>
-                        </CardStyle__description>
-                        <CardStyle__information>
+                        </div>
+                        <div className="Slider__information">
                            <h3>Information</h3>
                            <ul>
                               <li>
@@ -147,19 +96,15 @@ const Slider = ({ datas }) => {
                                  <span>Def: </span> {def}
                               </li>
                            </ul>
-                        </CardStyle__information>
-                     </CardStyle__content>
-                  </CardStyle>
+                        </div>
+                     </div>
+                  </div>
                ),
             )}
-         </SliderStyle>
+         </div>
          <ButtonsStyle>
-            <button id="previous" onClick={handleClick}>
-               Anterior
-            </button>
-            <button id="next" onClick={handleClick}>
-               Proximo
-            </button>
+            <button onClick={previousSlider}>Anterior</button>
+            <button onClick={nextSlider}>Proximo</button>
          </ButtonsStyle>
       </SectionStyle>
    );
